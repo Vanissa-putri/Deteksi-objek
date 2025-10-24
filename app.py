e0f023ec899938a4eb2062f6d74a9c7b95c0abd7
Vanissa Aulya Putri <vanissaputri88@gmail.com>
2025-10-24 13:58:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ KERAS_MODEL_PATH = "model/Vanissa Aulya Putri_Laporan 2.h5"
 WHATSAPP_NUMBER = "6282245357681"
 EMAIL_WATERVISION = "watervision@gmail.com"
 BG_IMAGE = "assets/water_bg.jpg"
-HERO_IMAGE = "assets/botol_air.jpg"
+HERO_IMAGE = "assets/botol_air.png"
 
 # ==============================
 # ADD CSS (DARK/LIGHT MODE + BG)
